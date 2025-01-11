f8a20ad2dc3c0cb9e4b58e26a174897209e90e96
Aneesh165 <aneeshmp165@gmail.com>
2025-01-12 01:26:37 +0530

    app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Design from './pages/Design'
+
+const App = () => {
+  return (
+    <div className='bg-[#0b4462] h-[100vh] flex justify-center items-center'>
+      <Design/>
+    </div>
+  )
+}
+
+export default App
