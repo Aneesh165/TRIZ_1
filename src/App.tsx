cc41f7ec2066d3a235bf4dea639388f9aaa084fa
Aneesh165 <aneeshmp165@gmail.com>
2025-01-12 12:00:47 +0530

    a

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Design from './pages/Design'
 
 const App = () => {
   return (
-    <div className='bg-[#0b4462] min-h-screen flex justify-center items-center overflow-y-hidden'>
+    <div className='bg-[#0b4462] min-h-screen flex justify-center items-center overflow-hidden'>
       <Design/>
     </div>
   )
